24a0fdb98c7ec2c0e3cd89eeb4b19e45a568e765
Omar27522 <m1i6g5u9e8l2@gmail.com>
2024-09-15 22:00:42 -0700

    resizer
    :-1:

---
+++ image_converter_resizer/resizer/index.php
@@ -0,0 +1,28 @@
+<?php
+list($width, $height, $type, $attr) = getimagesize("flag.jpg");
+echo "<img src=\"flag.jpg\" $attr alt=\"getimagesize() example\" />";
+?>
+<hr>
+<img src="flag.jpg" alt="image">
+
+
+<?php
+// Load the image
+$source = imagecreatefromjpeg('flag.jpg');
+list($width, $height) = getimagesize('flag.jpg');
+
+// Define new dimensions (200x200 pixels)
+$newWidth = 500;
+$newHeight = 200;
+
+// Create a new image
+$thumb = imagecreatetruecolor($newWidth, $newHeight);
+
+// Resize
+imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+// Save the resized image
+imagejpeg($thumb, 'resized_image2.jpg', 100);
+?>
+
+<!--Thanks to: https://cloudinary.com/guides/bulk-image-resize/resize-image-in-php-a-developers-guide-to-efficient-image-manipulation -->
